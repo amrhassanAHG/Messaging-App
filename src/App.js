d1262ecab492bf6ba8582796db65eb93aad6303f
amrhassanAHG <amrhassanAHG@gmail.com>
2022-01-20 11:08:58 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import { ChatEngine } from 'react-chat-engine'
+
+const App = () => {
+  return (
+    <ChatEngine 
+        height="100vm"
+        projectID=""
+        userName=""
+        userSecret=""
+    />
+  );
+};
+
+export default App;
