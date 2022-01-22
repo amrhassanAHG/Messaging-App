f9368f9e3a8ed787dace212fbf892efe9c812ab2
amrhassanAHG <amrhassanAHG@gmail.com>
2022-01-22 14:47:44 +0200

    render caht feed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.css'
-import { ChatEngine } from 'react-chat-engine'
+import "./App.css";
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
 
 const App = () => {
   return (
-    <ChatEngine 
-        height="100vm"
-        projectID=""
-        userName=""
-        userSecret=""
+    <ChatEngine
+      height="100vh"
+      projectID="182eb837-42db-4477-8586-c8d7b137c83c"
+      userName="amrhassanAHG"
+      userSecret="123123"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
 };
---
+++ src/components/MyMessage.jsx
@@ -0,0 +1,30 @@
+import { Markup } from "interweave";
+
+const MyMessage = ({ message }) => {
+  if (message?.attachments?.length > 0) {
+    return (
+      <img
+        src={message.attachments[0].file}
+        alt="message-attatchment"
+        className="message-image"
+        style={{ float: "right" }}
+      />
+    );
+  }
+
+  return (
+    <div
+      className="message"
+      style={{
+        float: "right",
+        marginRight: "18px",
+        color: "white",
+        backgroundColor: "#3B2A50",
+      }}
+    >
+      <Markup content={message.text} />
+    </div>
+  );
+};
+
+export default MyMessage;
